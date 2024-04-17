17ae06ebd29c9bb7a9d6975a177049ee04319c42
Igor Bessa <ibessa@ucsc.edu>
2024-04-17 10:50:00 -0700

    starting helper functions

--- Generator_Project.js
+++ Generator_Project.js
@@ -1,53 +1,57 @@
 let map = [];
+let space = 10;     // space will determine the space between characters
  
  function setup() {
   createCanvas(640, 480);
   //randomSeed(0);    // set a random seed 
-  background(random(50,100), random(50,100), random(175,255));
+  //background(random(50,100), random(50,100), random(175,255));
   fill(0);
   strokeWeight(3);
   rectMode(CENTER);
   textSize(20);
 
-  let space = 10;     // space will determine the space between characters
-  //let ascii = ["╖", "╕", "╣", "║", "╗", "╝", "╜", "╛", "  ╡"]
-  let ascii = ["X", "m", "-", "o", "O", "~", "s", "π", "φ", "∩", "£"];
+  let ascii = ["X", "m", "o", "O", "~", "s", "π", "φ", "∩", "£"];  // this will be the list of environment objects
+
   for(x = 0; x < width; x++){
     map[x] = [];
     for(y = 0; y < height; y++){    
-      // text("-", x, y);
-
-      //console.log("current x: ",x, " current row: ", map[x])
       map[x][y] = "-";
     }
   }
-  let r, c = 0;
-  //console.log(map[0]);
-  for(x = 0; x < width; x += space){
+  displayMap(map);
+}
 
-    for(y = 0; y < height; y += space){
-      //console.log(map[r]);
-      text(map[c][r], x, y);
-      if(c < height){
-        c++;
-      }
-      else{
-        c = 0;
-      }
-      
-    }
-    if(r < width){
-      r++;
-    }
-    else{
-      r = 0;
-    }
-  }
+function mutate(map){
+
 
 }
 
-function displayMap(){
+function check_neighbors(map){
+  
+}
 
+function displayMap(map){
+  let r = 0;
+  let c = 0;
+  for(x = 0; x < width; x += space){
+    for(y = 0; y < height; y += space){
+      let symbol = map[r][c];
+      text(symbol, x, y);
+      if(r < map[x].length){
+        r++;
+      }
+      else{
+        r = 0;
+      }
+      
+    }
+    if(c < map.length){
+      c++;
+    }
+    else{
+      c = 0;
+    }
+  }
 }
 
 function draw() {
